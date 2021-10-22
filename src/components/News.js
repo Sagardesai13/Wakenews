b4241d105bb7cb6a980f5ac2015b41ca5a3b1d11
Sagar Desai <desai.sagar2002@gmail.com>
2021-10-22 14:46:36 +0530

    Fetch News API.

---
+++ src/components/News.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            articles: [],
+            loading: false,
+            page: 1
+        }
+    }
+
+    async componentDidMount() {
+
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=fdd4e3a4d28a4dae8d4f980804258026";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+    }
+
+    handlePrevClick = async () => {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=fdd4e3a4d28a4dae8d4f980804258026&page=${this.state.page - 1}&pageSize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+            let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=fdd4e3a4d28a4dae8d4f980804258026&page=${this.state.page + 1}&pageSize=20";
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="container my-3">
+                <h2>Wakenews - Top Headlines</h2>
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                    })
+                    }
+                </div >
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
+            </div >
+        )
+    }
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl } = this.props;
+
+        return (
+            <div className="container my-3">
+                <div class="card" style={{ width: "18rem" }}>
+                    <img src={!imageUrl ? "https://indiaeducationdiary.in/wp-content/uploads/2020/08/Default-Image-IED.png" : imageUrl} className="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">{title}</h5>
+                        <p class="card-text">{description}</p>
+                        <a href={newsUrl} target="_blank" class="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}

30112aca952b723f1b8c6db04453c93a45a9ccec
Sagar Desai <desai.sagar2002@gmail.com>
2021-10-24 15:36:24 +0530

    Add Infinite Loop

--- src/components/News.js
+++ src/components/News.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-import Loading from './Loading'
+import Loading from './Loading';
 import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export default class News extends Component {
-
+export class News extends Component {
     static defaultProps = {
         country: 'in',
         pageSize: 8,
@@ -16,61 +16,90 @@ export default class News extends Component {
         pageSize: PropTypes.number,
         category: PropTypes.string,
     }
-
-    constructor() {
-        super();
+    capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
-            loading: false,
-            page: 1
+            loading: true,
+            page: 1,
+            totalResults: 0
         }
+        document.title = `Wakenews | ${this.capitalizeFirstLetter(this.props.category)}`;
     }
 
-    async updateNews(){
+    async updateNews() {
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fdd4e3a4d28a4dae8d4f980804258026&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
-        articles: parsedData.articles,
-        totalResults: parsedData.totalResults,
-        loading: false
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loading: false, 
         })
-        }
-        
 
+    }
     async componentDidMount() {
         this.updateNews();
     }
 
-    handlePrevClick = async () => {
-        this.setState({page: this.state.page - 1});
+    /*handlePrevClick = async () => {
+        this.setState({ page: this.state.page - 1 });
         this.updateNews();
     }
 
     handleNextClick = async () => {
-        this.setState({page: this.state.page + 1});
-        this.updateNews();
-    }
+        this.setState({ page: this.state.page + 1 });
+        this.updateNews()
+    }*/
+
+    fetchMoreData = async () => {  
+        this.setState({page: this.state.page + 1})
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fdd4e3a4d28a4dae8d4f980804258026&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults
+        })
+      };
 
     render() {
         return (
-            <div className="container my-3">
-                <h2 className="text-center" style={{margin: '35px 0px'}}>Wakenews - Top Headlines</h2>
+            <>
+                <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Loading />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
-                        </div>
-                    })
-                    }
-                </div >
-                <div className="container d-flex justify-content-between">
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Loading/>}
+                > 
+                    <div className="container">
+                         
+                   <div className="row">
+                        {this.state.articles.map((element) => {
+                            return <div className="col-md-4" key={element.url}>
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                            </div>
+                        })}
+                    </div> 
+                    </div> 
+                </InfiniteScroll>
+
+                {/*<div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
                     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
-            </div >
+                </div>*/}
+                
+            </>
+           
         )
+         
     }
 }
+
+export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -12,7 +12,7 @@ export default class NewsItem extends Component {
                         <h5 className="card-title">{title}</h5>
                         <span className="badge bg-success">{source}</span>
                         <p className="card-text">{description}</p>
-                        <p className="card-text"><small class="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+                        <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer"  href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>

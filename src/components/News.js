405ee88d7e3af88dcd67739f1d84dfe1ef038531
Sagar Desai <desai.sagar2002@gmail.com>
2021-10-25 15:46:22 +0530

    Complete Class based Wakenews

--- src/components/News.js
+++ src/components/News.js
@@ -74,7 +74,7 @@ export class News extends Component {
     render() {
         return (
             <>
-                <h1 className="text-center" style={{ margin: '35px 0px' }}>Wakenews - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>Wakenews - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Loading />}
                 <InfiniteScroll
                     dataLength={this.state.articles.length}
